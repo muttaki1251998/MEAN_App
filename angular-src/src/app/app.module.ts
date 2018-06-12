e659bf05238e10c9a316a4520c22e2be9f353aac
muttaki1251998 <muttaki4989@gmail.com>
2018-06-12 06:49:05 +0600

    Validation serivice created

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -9,6 +10,9 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+
+import { ValidateService } from "./services/validate.service";
+import { FlashMessagesModule } from "angular2-flash-messages";
 
 const appRoutes: Routes = [
   {path: "", component: HomeComponent},
@@ -30,9 +34,10 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule, FlashMessagesModule.forRoot()
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+
+
+  constructor(private _validateService: ValidateService, private _flashMsg: FlashMessagesService) { }
 
   ngOnInit() {
   }
+
+  OnRegister(){
+    const user = {
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password
+    }
+
+  // Verify user
+  if(!this._validateService.validateRegister(user)){
+    this._flashMsg.show(("Fill in all the fields"), {cssClass: "alert-danger", timeout: 3000}); 
+    return false;
+  }
+
+  // Verify email
+  if(!this._validateService.validateEmail(user.email)){
+    this._flashMsg.show(("Enter a valid email"), {cssClass: "alert-danger", timeout: 3000});
+    return false;
+  }
+
+}
+
+  
 
 }
