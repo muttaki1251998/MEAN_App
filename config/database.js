782fd81aaf7913bd08ed65ee274c8eb4436a1f1a
muttaki1251998 <muttaki4989@gmail.com>
2018-06-15 00:31:10 +0600

    Db linked

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: "mongodb://localhost:27017/meandb",
+    database: "mongodb://muttaki1251998:salamence1251998;@ds245680.mlab.com:45680/tasklists_muttaki",
     secret: "sherin"
 }
