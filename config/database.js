2dbfd21758db3ac33a7f9e682774b1537150b816
muttaki1251998 <muttaki4989@gmail.com>
2018-06-15 12:48:03 +0600

    Heroku db added

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: "mongodb://muttaki1251998:Salamence1251998;@ds159100.mlab.com:59100/meandb",
+    database: "mongodb://muttaki:sherin123@ds159100.mlab.com:59100/meandb",
     secret: "sherin"
 }
